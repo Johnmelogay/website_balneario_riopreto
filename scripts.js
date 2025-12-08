90e081c63fe132f4ede3b8e5c0030ecbd5c13f02
João Melo <johnmelocontato@gmail.com>
2025-12-08 19:55:19 -0400

    Fix: simplified share button to only copy link

--- scripts.js
+++ scripts.js
@@ -1,6 +1,7 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { createClient } from 'https://esm.sh/@supabase/supabase-js@2';
 
 const SUPABASE_URL = 'https://hihaipaslnpaqnqotrwm.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpaGFpcGFzbG5wYXFucW90cndtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQyNjE1OTIsImV4cCI6MjA3OTgzNzU5Mn0.zwPHKlcYNQnlQbQdf83qbH3mk4Dsc8fVF4NfWDBs_LA';
+const SUPABASE_ANON_KEY =
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhpaGFpcGFzbG5wYXFucW90cndtIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc2NDI2MTU5MiwiZXhwIjoyMDc5ODM3NTkyfQ.mJ6Rvbe8QWPesfgLdmhXLXTg1eBRao6hgr0OUXXzJPw';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
